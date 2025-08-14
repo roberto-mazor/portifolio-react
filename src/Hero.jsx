cfba1da07f3f37395d2021f8f70e215d5103de34
roberto-mazor <mazor.estudo@gmail.com>
2025-08-13 21:35:07 -0300

    Fix: Corrigido url image perfil

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -6,7 +6,7 @@ function Hero (){
             <section className="hero" id="inicio">
             <div className="hero-content">
                 <h2>Bem-vindo ao meu portfólio!</h2>
-                <img src="./public/perfil.jpeg" alt="Foto do Programador"/>
+                <img src="/perfil.jpeg" alt="Foto do Programador"/>
                 <p>Desenvolvedor Front-End especializado em criar experiências
                     web incríveis.</p>
                 <div className="links-rapidos">
